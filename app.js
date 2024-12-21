7432e956dd66c46f1921c0051cefb0aca170e66d
Nida EKİNCİ <l21110120191@stud.sdu.edu.tr>
2024-12-22 01:46:04 +0300

    updated2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//const cors = require('cors')   //
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,6 +8,18 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRoutes = require('./app_api/routes/index');
 var app = express();
+
+/*
+app.use(cors(
+    {
+        origin: ["https://backend-3j3jad668-nida-ekincis-projects.vercel.app/"],
+        methods: ["POST", "GET"],
+        credential: true
+    }
+))
+*/
+
+//app.use(express.json())  //
 
 app.use(logger('dev'));
 app.use(express.json());
